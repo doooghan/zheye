1596b7c161bdf361fcb4a021bd21d7bd900c2b80
doooghan <doooghan@foxmail.com>
2022-12-05 18:04:00 +0800

    dropdown feature

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,18 +1,32 @@
 <script setup lang="ts">
-import { ref, toRefs } from 'vue';
-
+import { onMounted, onUnmounted, ref, toRefs } from 'vue';
 
 const props = defineProps<{ title: string }>()
 const isOpen = ref(false)
+const dropdownRef = ref<HTMLElement | null>(null)
 const toggleOpen = () => {
   isOpen.value = !isOpen.value
 }
+
+const handler = (e: MouseEvent) => {
+  if (dropdownRef.value) {
+    if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
+      isOpen.value = false
+    }
+  }
+}
+onMounted(() => {
+  document.addEventListener('click', handler)
+})
+onUnmounted(() => {
+  document.removeEventListener('click', handler)
+})
 
 const { title } = toRefs(props)
 </script>
 
 <template>
-  <div class="dropdown">
+  <div class="dropdown" ref="dropdownRef">
     <a href="#" class="btn btn-outline-light my-2 dropdown-toggle" @click.prevent="toggleOpen">{{ title }}</a>
     <ul class="dropdown-menu" style="display: block;" v-if="isOpen">
       <slot></slot>
