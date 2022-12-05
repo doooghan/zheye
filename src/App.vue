7ba863b87175bf9891bf9828c53e3581bacd1c5e
doooghan <doooghan@foxmail.com>
2022-12-05 19:24:12 +0800

    基础form

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import ColumnList, { ColumnProps } from './components/ColumnList.vue';
 import GlobalHeader, { UserProps } from './components/GlobalHeader.vue';
 import vueSvg from './assets/vue.svg'
+import { reactive } from 'vue';
 
 const currentUser: UserProps = {
   isLogin: true,
@@ -9,7 +10,21 @@ const currentUser: UserProps = {
   id: 123
 }
 
-
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+const emailRef = reactive({
+  val: '',
+  error: false,
+  message: ''
+})
+const validateEmail = () => {
+  if (emailRef.val.trim() === '') {
+    emailRef.error = true
+    emailRef.message = '请输入邮箱'
+  } else if (!emailReg.test(emailRef.val)) {
+    emailRef.error = true
+    emailRef.message = '请输入有效的邮箱'
+  }
+}
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -41,7 +56,20 @@ const testData: ColumnProps[] = [
 <template>
   <div class="container">
     <GlobalHeader :user="currentUser" />
-    <ColumnList :list="testData" />
+    <!-- <ColumnList :list="testData" /> -->
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+          @blur="validateEmail" v-model="emailRef.val">
+        <div id="emailHelp" class="form-text" v-if="emailRef.error">{{ emailRef.message }}</div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input type="password" class="form-control" id="exampleInputPassword1">
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </div>
 </template>
 
