13aa8610842bb6eaaaeb01a148c67b710f874602
doooghan <doooghan@foxmail.com>
2022-12-07 23:36:49 +0800

    proxy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.vue";
 import { router } from "./router";
 import { pinia } from "@/stores";
+import axios from "axios";
+
+axios.defaults.baseURL = "/api/";
+axios.get("/columns?currentPage=1&pageSize=6").then((resp) => {
+	console.log(resp.data);
+});
 
 const app = createApp(App);
 app.use(router);
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: './',
-  resolve:{
-    alias:{
-      "@": path.join(__dirname, "./src")
-    }
-  }
-})
+	plugins: [vue()],
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://api.vikingship.xyz/",
+				// 是否允许跨域,在本地会创建一个虚拟服务端，然后发送请求的数据
+				changeOrigin: true,
+			},
+		},
+	},
+
+	base: "./",
+	resolve: {
+		alias: {
+			"@": path.join(__dirname, "./src"),
+		},
+	},
+});
