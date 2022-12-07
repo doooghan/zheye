6073582b3f7852c9256fa62efe3f555bbb4d05c8
doooghan <doooghan@foxmail.com>
2022-12-07 16:49:00 +0800

    router meta

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,15 +3,37 @@ import HomeVue from "@/views/Home.vue";
 import LoginVue from "@/views/Login.vue";
 import ColumnDetailVue from "@/views/ColumnDetail.vue";
 import CreatePostVue from "@/views/CreatePost.vue";
+import { useMainStore } from "@/stores";
 
 const routes: RouteRecordRaw[] = [
 	{ path: "/", name: "Home", component: HomeVue },
-	{ path: "/login", name: "Login", component: LoginVue },
+	{
+		path: "/login",
+		name: "Login",
+		component: LoginVue,
+		meta: { redirectAlreadyLogin: true },
+	},
 	{ path: "/column/:id", name: "ColumnDetail", component: ColumnDetailVue },
-	{ path: "/create", name: "CreatePost", component: CreatePostVue },
+	{
+		path: "/create",
+		name: "CreatePost",
+		component: CreatePostVue,
+		meta: { requiredLogin: true },
+	},
 ];
 
 export const router = createRouter({
 	history: createWebHistory(),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+	const store = useMainStore();
+	if (to.meta.requiredLogin && !store.user.isLogin) {
+		next({ name: "Login" });
+	} else if (to.meta.redirectAlreadyLogin && store.user.isLogin) {
+		next({ name: "Home" });
+	} else {
+		next();
+	}
 });
